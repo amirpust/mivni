9860059cc07e7b42c608fe1b7ca5a1e07923bc92
amirpust <54210536+amirpust@users.noreply.github.com>
2019-11-29 13:23:49 +0200

    Node class

---
+++ Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Amir on 29/11/2019.
+//
+
+#include "Node.h"
---
+++ Node.h
@@ -0,0 +1,18 @@
+//
+// Created by Amir on 29/11/2019.
+//
+
+#ifndef _NODE_H
+#define _NODE_H
+template <class T>
+class Node{
+private:
+    T data;
+    Node* next;
+    Node* previous;
+public:
+
+
+};
+
+#endif //_NODE_H
