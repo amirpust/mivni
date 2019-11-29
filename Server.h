3c650e2f40406ebf836783ecbfa4ce1b910e5e69
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2019-11-29 14:28:06 +0200

    Server almost done

--- Server.h
+++ Server.h
@@ -19,18 +19,45 @@ public:
 
     ~Server() = default;
 
+    /**
+     * The Function switch between OS's and already remove/add it from/to the new list.
+     * Need the new list ptr's - can be nullptr (that's why it need both)
+     * @param newNext: the new next ptr for the new list
+     * @param newPrev: the new previous ptr for the new list
+     */
     void switchOs (Server* newNext, Server* newPrev){
-        removeNodeFromList();
+        if(taken)
+            return; //TODO: throw
+        removeServerFromList();
         os = 1 - os;
-
+        addServerToList(newNext,newPrev);
+        taken = true;
+    }
+    int getOs(){
+        return os;
+    }
+    bool isTaken(){
+        return taken;
+    }
+    void setTaken(bool _taken){
+        this->taken = _taken;
     }
 
+
 private:
-    void removeNodeFromList(){
+    void removeServerFromList(){
         if (previous != nullptr)
             previous->next = next;
         if (next != nullptr)
             next->previous = previous;
+    }
+    void addServerToList(Server* newNext, Server* newPrev){
+        next = newNext;
+        previous = newPrev;
+        if(newNext != nullptr)
+            newNext->previous=this;
+        if(newPrev != nullptr)
+            newPrev->next=this;
     }
 };
 
