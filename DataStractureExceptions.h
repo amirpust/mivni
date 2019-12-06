14624c0cd36b48d57ea897e17fdafbd83baf6d59
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2019-12-06 19:01:09 +0200

    AVLTree near done

--- DataStractureExceptions.h
+++ DataStractureExceptions.h
@@ -2,14 +2,43 @@
 #define _DATASTRACTUREEXCEPTIONS_H
 
 #include <exception>
+#include "library1.h"
 
 using std::exception;
 
-class OutOfMemory : public exception{};
-class AlreadyExists: public exception{};
-class InvalidInput: public exception{};
-class NullArg: public exception{};
-class DoesntExists: public exception{};
+class OutOfMemory : public exception{
+public:
+    StatusType statusType = ALLOCATION_ERROR;
+};
 
+class AlreadyExists: public exception{
+public:
+    StatusType statusType = FAILURE;
+};
+
+class InvalidInput: public exception{
+public:
+    StatusType statusType = INVALID_INPUT;
+};
+
+class NullArg: public exception{
+public:
+    StatusType statusType = FAILURE;
+};
+
+class DoesntExists: public exception{
+public:
+    StatusType statusType = FAILURE;
+};
+
+class NoFreeServers: public exception{
+public:
+    StatusType statusType = FAILURE;
+};
+
+class AlreadyFree: public exception{
+public:
+    StatusType statusType = FAILURE;
+};
 
 #endif //_DATASTRACTUREEXCEPTIONS_H
