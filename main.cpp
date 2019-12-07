adade5ea264ecfec03c04e95ed10f91060866f08
amirpust <54210536+amirpust@users.noreply.github.com>
2019-12-07 17:00:40 +0200

    exception throws

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "DataStructure.h"
+using namespace std;
+
+int main(){
+    auto ds = new DataStructure();
+    if(ds->addDataCenter(456,2)== SUCCESS){
+        cout << "success" <<endl;
+    }else{
+        cout << "check add data center"<<endl;
+    }
+
+    int Id = 0;
+    if(ds->requestServerFromDataCenter(456,0,1,&Id) == SUCCESS){
+        cout << "success" <<endl;
+    }else{
+        cout << "check request server"<< endl;
+    }
+    return 0;
+};
